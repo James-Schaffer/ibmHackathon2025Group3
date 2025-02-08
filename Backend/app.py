6d86dd2612566e9f2b2fe75f105856a925f76140
YS403 <yahyasakir639@gmail.com>
2025-02-08 13:56:49 +0000

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask,render_template,request
-
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def Camera():
     if request.method == 'POST': 
-        print(1)
+        image = request.files['image']
+        # print(image.filename)
+        image.save(os.path.join("media/", secure_filename(image.filename)))
 
     return render_template("index.html")
 
